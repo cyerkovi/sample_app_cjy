9059e5ff87a55d83f17bcd0d8ed4085530cd8b5d
Carl Yerkovich <cyerkovi@yahoo.com>
2012-06-23 13:02:52 -0700

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,11 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -34,4 +40,6 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+   
 end
